c252d2954e5b267400d717fb15e54d3605023e01
Pranavbc <pranav1952000@gmail.com>
2025-04-25 11:44:35 +0530

    c

---
+++ UserService/src/main/java/com/lcwd/user/service/entities/Rating.java
@@ -0,0 +1,59 @@
+package com.lcwd.user.service.entities;
+
+public class Rating {
+    private String ratingId;
+    private String userId;
+
+    private  String hotelId;
+    private int ratings;
+
+    private String feedback;
+//
+//    public Rating(String ratingId, String userId, String hotelId, int ratings, String feedback) {
+//        this.ratingId = ratingId;
+//        this.userId = userId;
+//        this.hotelId = hotelId;
+//        this.ratings = ratings;
+//        this.feedback = feedback;
+//    }
+//
+//    public String getRatingId() {
+//        return ratingId;
+//    }
+//
+//    public void setRatingId(String ratingId) {
+//        this.ratingId = ratingId;
+//    }
+//
+//    public String getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+//
+//    public String getHotelId() {
+//        return hotelId;
+//    }
+//
+//    public void setHotelId(String hotelId) {
+//        this.hotelId = hotelId;
+//    }
+//
+//    public int getRatings() {
+//        return ratings;
+//    }
+//
+//    public void setRatings(int ratings) {
+//        this.ratings = ratings;
+//    }
+//
+//    public String getFeedback() {
+//        return feedback;
+//    }
+//
+//    public void setFeedback(String feedback) {
+//        this.feedback = feedback;
+//    }
+}
